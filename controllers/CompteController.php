a5b0112d2d3bb9854cd5bb5e3d388a4e9a5c61fd
gAb09 <brunogabiot@club-internet.fr>
2015-02-21 19:52:25 +0100

    Debug et renommage mise en session de la classe PCG en cours de consultation

--- controllers/CompteController.php
+++ controllers/CompteController.php
@@ -17,13 +17,13 @@ class CompteController extends BaseController {
 
 	public function index($choix = null)
 	{
-		$numero = (is_null($choix)) ? Session::get('Courant.classe') : $choix ;
+		$numero = (is_null($choix)) ? Session::get('ParamEnv.tresorerie.classePCG') : $choix ;
 
 		/* Passer en session le numero,
 		pour mémoriser la classe sur laquelle
 		l’utilisateur est en cours de travail
 		et la mémoriser au fil de la navigation. */
-		Session::set('Courant.classe', $numero);
+		Session::put('ParamEnv.tresorerie.classePCG', $numero);
 
 		/* Assigner la liste des racines de comptes (classes) 
 		pour le tableau de sélection des classes */
